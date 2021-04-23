c58e58fb3fa59cc3e40cc140de733f7eeefebbd3
emmanuel <emmajnrk@gmail.com>
2021-04-23 23:16:29 +0100

    add createdAt to note

--- src/pages/writer.jsx
+++ src/pages/writer.jsx
@@ -65,13 +65,58 @@ export default function NewNote() {
   const handleMessageChange = (e) => {
     setMessage(e.target.value);
   };
+  function getDate() {
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let today = new Date();
+    var day = today.getDate();
+    let month = monthNames[today.getMonth()];
+    let year = today.getFullYear();
+
+    function ordinalSuffixOfDay(i) {
+      var j = i % 10,
+        k = i % 100;
+      if (j === 1 && k !== 11) {
+        return i + "st";
+      }
+      if (j === 2 && k !== 12) {
+        return i + "nd";
+      }
+      if (j === 3 && k !== 13) {
+        return i + "rd";
+      }
+      return i + "th";
+    }
+
+    return `${ordinalSuffixOfDay(day)} ${month}, ${year}`;
+  }
   const saveNote = () => {
     // let _note = notes;
     // console.log(_note);
     if (title || message) {
       setNewNote((oldList) => {
         // debugger;
-        return [{ id: makeid(5), title: title, message: message }, ...oldList];
+        return [
+          {
+            id: makeid(5),
+            title: title,
+            message: message,
+            createdAt: getDate(),
+          },
+          ...oldList,
+        ];
       });
       setMessage("");
       setTitle("");
